c1f74230bfc35e84e6ee8715ece02d4eab082c5c
adenadams20 <adamsdiene@gmail.com>
2026-03-11 22:46:07 +0000

    mode-prod

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ useEffect(() => {
             setLoading(true);
 
             const API_URL = import.meta.env.VITE_API_URL;
-
+console.log("API URL:", API_URL);
             const [braceletRes, bestSellerRes, perruqueRes] = await Promise.all([
                 fetch(`${API_URL}/products?category=bracelet`),
                 fetch(`${API_URL}/products?category=bestseller`),
